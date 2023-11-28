bcba1fad4431a56cc47e8b41028541b75b507358
Official0mega <supr3m30m3ga@gmail.com>
2023-11-28 21:58:44 +0100

    returned promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+// Returning a Promise//
+// 0. Keep every promise you make and only make promises you can keep
+export default function getResponseFromAPI() {
+    return new Promise(() => {
+
+    });
+};
